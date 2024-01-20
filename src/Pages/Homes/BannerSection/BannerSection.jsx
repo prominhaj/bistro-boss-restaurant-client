0337fbcf8de3794328d4127b8215740f3506a9e7
Md Minhaj <parsonal322532@gmail.com>
2024-01-21 01:59:27 +0600

    Implement Banner Section Items Map

--- src/Pages/Homes/BannerSection/BannerSection.jsx
+++ src/Pages/Homes/BannerSection/BannerSection.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './BannerSection.css';
+import "./BannerSection.css";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import bannerImg1 from "../../../assets/home/01.jpg";
@@ -9,30 +9,52 @@ import bannerImg4 from "../../../assets/home/04.jpg";
 import bannerImg5 from "../../../assets/home/05.png";
 import bannerImg6 from "../../../assets/home/06.png";
 
+// Banner Items Data
+const bannerItems = [
+  {
+    _id: 1,
+    img: bannerImg1,
+  },
+  {
+    _id: 2,
+    img: bannerImg2,
+  },
+  {
+    _id: 3,
+    img: bannerImg3,
+  },
+  {
+    _id: 4,
+    img: bannerImg4,
+  },
+  {
+    _id: 5,
+    img: bannerImg5,
+  },
+  {
+    _id: 6,
+    img: bannerImg6,
+  },
+];
 
 const BannerSection = () => {
-    
   return (
     <div className="">
-      <Carousel autoPlay interval={5000} emulateTouch={true} infiniteLoop={true} useKeyboardArrows={true} stopOnHover={true} thumbWidth={100} dynamicHeight={true}>
-        <div>
-          <img className="xl:h-[85vh]" src={bannerImg1} />
-        </div>
-        <div>
-          <img className="xl:h-[85vh]" src={bannerImg2} />
-        </div>
-        <div>
-          <img className="xl:h-[85vh]" src={bannerImg3} />
-        </div>
-        <div>
-          <img className="xl:h-[85vh]" src={bannerImg4} />
-        </div>
-        <div>
-          <img className="xl:h-[85vh]" src={bannerImg5} />
-        </div>
-        <div>
-          <img className="xl:h-[85vh]" src={bannerImg6} />
-        </div>
+      <Carousel
+        autoPlay
+        interval={5000}
+        emulateTouch={true}
+        infiniteLoop={true}
+        useKeyboardArrows={true}
+        stopOnHover={true}
+        thumbWidth={100}
+        dynamicHeight={true}
+      >
+        {bannerItems.map((item) => (
+          <div key={item._id}>
+            <img className="xl:h-[85vh]" src={item.img} />
+          </div>
+        ))}
       </Carousel>
     </div>
   );
