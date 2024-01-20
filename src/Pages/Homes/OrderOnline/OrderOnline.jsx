11f8f43030e8f45ccc98df2eaefa6687615bad22
Md Minhaj <parsonal322532@gmail.com>
2024-01-21 01:49:39 +0600

    Implement Order Online With Carousel Using By Swiperjs

--- src/Pages/Homes/OrderOnline/OrderOnline.jsx
+++ src/Pages/Homes/OrderOnline/OrderOnline.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import { Swiper, SwiperSlide } from "swiper/react";
+import "./OrderOnline.css";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
@@ -14,6 +15,41 @@ import slideImg3 from "../../../assets/home/slide3.jpg";
 import slideImg4 from "../../../assets/home/slide4.jpg";
 import slideImg5 from "../../../assets/home/slide5.jpg";
 
+// Order Online Data
+const orderItems = [
+  {
+    itemName: "Salads",
+    itemImg: slideImg1,
+    id:1
+  },
+  {
+    itemName: "Soups",
+    itemImg: slideImg2,
+    id:2
+  },
+  {
+    itemName: "Soups",
+    itemImg: slideImg1,
+    id:6
+  },
+  {
+    itemName: "pizzas",
+    itemImg: slideImg3,
+    id:3
+  },
+  {
+    itemName: "Salads",
+    itemImg: slideImg5,
+    id:5
+  },
+  {
+    itemName: "desserts",
+    itemImg: slideImg4,
+    id:4
+  },
+ 
+];
+
 const OrderOnline = () => {
   return (
     <div className="container mx-auto py-[50px] px-5">
@@ -23,9 +59,10 @@ const OrderOnline = () => {
           spaceBetween={30}
           slidesPerView={"auto"}
           effect={"coverflow"}
+          loop={true}
           grabCursor={true}
           autoplay={{
-            delay: 2500,
+            delay: 2000,
             disableOnInteraction: false,
           }}
           centeredSlides={true}
@@ -58,54 +95,16 @@ const OrderOnline = () => {
           }}
         >
           {/* items */}
-          <SwiperSlide>
-            <div className="relative w-full h-[400px] xl:h-[450px]">
-              <img className="-z-10 w-full h-full" src={slideImg1} alt="" />
-              <h4 className="absolute bottom-0 z-10 w-full text-center text-white text-[32px] font-normal font-['Cinzel']">
-                Salads
-              </h4>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="relative w-full h-[400px] xl:h-[450px]">
-              <img className="-z-10 w-full h-full" src={slideImg2} alt="" />
-              <h4 className="absolute bottom-0 z-10 w-full text-center text-white text-[32px] font-normal font-['Cinzel']">
-                Soups
-              </h4>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="relative w-full h-[400px] xl:h-[450px]">
-              <img className="-z-10 w-full h-full" src={slideImg3} alt="" />
-              <h4 className="absolute bottom-0 z-10 w-full text-center text-white text-[32px] font-normal font-['Cinzel']">
-                pizzas
-              </h4>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="rrelative w-full h-[400px] xl:h-[450px]">
-              <img className="-z-10 w-full h-full" src={slideImg4} alt="" />
-              <h4 className="absolute bottom-0 z-10 w-full text-center text-white text-[32px] font-normal font-['Cinzel']">
-                desserts
-              </h4>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="relative w-full h-[400px] xl:h-[450px]">
-              <img className="-z-10 w-full h-full" src={slideImg5} alt="" />
-              <h4 className="absolute bottom-0 z-10 w-full text-center text-white text-[32px] font-normal font-['Cinzel']">
-                Salads
-              </h4>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="relative w-full h-[400px] xl:h-[450px]">
-              <img className="-z-10 w-full h-full" src={slideImg2} alt="" />
-              <h4 className="absolute bottom-0 z-10 w-full text-center text-white text-[32px] font-normal font-['Cinzel']">
-                Soups
-              </h4>
-            </div>
-          </SwiperSlide>
+          {orderItems.map((item) => (
+            <SwiperSlide key={item.id}>
+              <div className="relative w-full h-[400px] xl:h-[450px]">
+                <img className="-z-10 w-full h-full" src={item.itemImg} alt="" />
+                <h4 className="absolute bottom-0 z-10 w-full text-center text-white text-[32px] font-normal font-['Cinzel']">
+                  {item.itemName}
+                </h4>
+              </div>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </div>
