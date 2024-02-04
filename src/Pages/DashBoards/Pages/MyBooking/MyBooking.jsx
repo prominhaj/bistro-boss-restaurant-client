23a57e438d7004bb59cd1e6d8eead7db994171ec
Md Minhaj <parsonal322532@gmail.com>
2024-02-04 22:54:35 +0600

    Implement User DashBoard My Bookings Page Table Add And Totalbookins Add Delete Action

--- src/Pages/DashBoards/Pages/MyBooking/MyBooking.jsx
+++ src/Pages/DashBoards/Pages/MyBooking/MyBooking.jsx
@@ -5,6 +5,8 @@ import DashBoardTable from "../../../Shared/DashBoardTable/DashBoardTable";
 import useAxiosSecure from "../../../../hook/useAxiosSecure/useAxiosSecure";
 import { useQuery } from "react-query";
 import useAuth from "../../../../hook/useAuth/useAuth";
+import swal from "sweetalert";
+import { FaRegTrashAlt } from "react-icons/fa";
 
 const MyBooking = () => {
   const [axiosSecure] = useAxiosSecure();
@@ -18,7 +20,72 @@ const MyBooking = () => {
     },
   });
 
-  console.log(allBookings);
+  const nameBodyTemp = (item, index) => {
+    return (
+      <div className="flex items-center gap-5">
+        <h2 className="text-neutral-900 text-xl font-bold font-['Inter']">
+          {index.rowIndex + 1}
+        </h2>
+        <h2>{item.name}</h2>
+      </div>
+    );
+  };
+
+  const guestBodyTemp = (item) => {
+    return (
+      <div>
+        <h2>{item.guest}</h2>
+      </div>
+    );
+  };
+
+  const timeBodyTemp = (item) => {
+    return (
+      <div>
+        <h2>{item.time}</h2>
+      </div>
+    );
+  };
+
+  const dateBodyTemp = (item) => {
+    return (
+      <div>
+        <h2>{item.date}</h2>
+      </div>
+    );
+  };
+
+  const actionBodyTemp = (item) => {
+    const handleDelete = (item) => {
+      swal({
+        title: "Are you sure?",
+        text: `You Wont Tow Delete Booking ${item.name}`,
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      }).then((willDelete) => {
+        if (willDelete) {
+          axiosSecure.delete(`/user-booking/${item._id}`).then((res) => {
+            if (res.data.deletedCount) {
+              refetch();
+              swal("Delete SuccessFull", {
+                icon: "success",
+              });
+            }
+          });
+        }
+      });
+    };
+
+    return (
+      <button
+        onClick={() => handleDelete(item)}
+        className="p-3 rounded bg-[#B91C1C]"
+      >
+        <FaRegTrashAlt className="text-2xl text-white" />
+      </button>
+    );
+  };
 
   return (
     <>
@@ -30,8 +97,23 @@ const MyBooking = () => {
       {/* Section Title */}
       <SectionTitle subHading="Excellent Ambience" hading="MY BOOKINGS" />
       <section className="py-10 md:px-10">
+        <h2 className="text-neutral-900 text-[32px] font-bold font-['Cinzel'] mb-4">
+          Total bookings: {allBookings.length}
+        </h2>
         <div>
-          <DashBoardTable data={allBookings} />
+          <DashBoardTable
+            data={allBookings}
+            header1="# Name"
+            header2="Guest"
+            header3="Time"
+            header4="Date"
+            header5="Action"
+            item1BodyTemp={nameBodyTemp}
+            item2BodyTemp={guestBodyTemp}
+            item3BodyTemp={timeBodyTemp}
+            item4BodyTemp={dateBodyTemp}
+            item5BodyTemp={actionBodyTemp}
+          />
         </div>
       </section>
     </>
